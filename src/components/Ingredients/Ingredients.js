6f14d37c0272ebf44507d31a5c25fab98d97ece5
Quinn94 <mayquinn94@gmail.com>
2020-05-02 20:15:06 +0800

    timer api delte

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,6 +8,7 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] =useState(false);
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', ingredients);
@@ -20,6 +21,7 @@ const Ingredients = () => {
   //browser function, send behind the scene http request. 2nd argument as a object that allows configuration. Firebase understand 'post' method not fetch. JSON is a class which takes stringify function to convert array/object to valid json format - a feature build into the browser. set headers that you want to append to the request. No need to do the same for Axios, because Axios did it for us (stringify & header)
   //response.json() will extract the response body, responseData will be return when the body has been extracted. ResponseData will be an object
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://react-hooks-fae2f.firebaseio.com/ingredients.json', { 
       method: 'POST',
       body: JSON.stringify(ingredient),
@@ -27,6 +29,7 @@ const Ingredients = () => {
     }).then(response => {
       return response.json();
     }).then(responseData => {
+      setIsLoading(false);
       setIngredients(prevIngredients => [
         ...prevIngredients, 
         {id: responseData.name, ...ingredient}
@@ -35,14 +38,20 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
-    setIngredients(prevIngredients => 
-      prevIngredients.filter(ig => ig.id !== ingredientId)
-    );
+    fetch(`https://react-hooks-fae2f.firebaseio.com/ingredients/${ingredientId}.json`, { 
+      method: 'DELETE'
+    }).then(response => {
+      setIngredients(prevIngredients => 
+        prevIngredients.filter(ig => ig.id !== ingredientId)
+      );
+    })
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm 
+      onAddIngredient={addIngredientHandler}
+      loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React , {useState, useEffect} from 'react';
+import React , {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,28 +6,37 @@ import './Search.css';
 const Search = React.memo(props => {
   const {onLoadIngredients} = props; //object destructuring, function is a object, onLoadIngredients as a key 
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
-  //to filter the ingredients in the backend, need to fetch backend data
   useEffect(() => {
-    //to only fetch information of 'enteredFilter' and not everything
-    const query = 
-      enteredFilter.length===0 
-      ? '' 
-      : `?orderBy="title"&equalTo="${enteredFilter}"`
-    fetch('https://react-hooks-fae2f.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData){
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
+    const timeout = setTimeout(() => {
+      //is the 'enteredFilter' the same as the 'enteredFilter' 500ms ago? the old 'enteredFilter' will be lock in when we set the timer. 'current' & 'value' property of useRef(). inputRef is defined outside of the closure, not locked in, hence it is the current value
+      if(enteredFilter === inputRef.current.value){
+        const query = 
+        enteredFilter.length===0 
+        ? '' 
+        : `?orderBy="title"&equalTo="${enteredFilter}"`
+        //to filter the ingredients in the backend, need to fetch backend data
+        //to only fetch information of 'enteredFilter' and not everything
+        fetch('https://react-hooks-fae2f.firebaseio.com/ingredients.json' + query)
+        .then(response => response.json())
+        .then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData){
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+          onLoadIngredients(loadedIngredients); //infinite loop, as parent will re-render when this function is being re-rendered, since this is a dependencies: solution useCallback() it cached your function for you, so that it survives re-render cycle
+        })
       }
-      onLoadIngredients(loadedIngredients); //infinite loop, as parent will re-render when this function is being re-rendered, since this is a dependencies: solution useCallback() it cached your function for you, so that it survives re-render cycle
-    })
-  },[enteredFilter, onLoadIngredients])
+    },500);
+    return () => {
+      clearTimeout(timeout); //useEffect() can return a function, clear-up function will run before the next time the 'above' function runs. hence it will clear all the old timer on every keystroke, leaving the final timer running
+    }
+  },[enteredFilter, onLoadIngredients, inputRef])
 
   return (
     <section className="search">
@@ -35,6 +44,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input 
+          ref={inputRef} //a special property supported by React, to connect useRef() to the DOM
           type="text"
           value={enteredFilter}
           onChange={event => setEnteredFilter(event.target.value)} />
